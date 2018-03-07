2bb43457ef5bc6ff111677cdbf4ca3b4ecdf3cf8
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-03-07 12:43:47 -0500

    add adjust result

---
+++ result/PASS-PASS/Decomposing/decomposing_B/remove_result.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -r compare* regression* result_img adjust_graph collision_graph retransmission_graph
+rm result/*.png
+rm result/time.txt
+rm index.html
+rm pickle*
+rm csv_data.txt data.csv
